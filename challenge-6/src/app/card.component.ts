7bade23566be2a2c5f3d5efd97cb24fb715c42d4
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-11-12 21:36:35 +0100

    update

--- challenge-6/src/app/card.component.ts
+++ challenge-6/src/app/card.component.ts
@@ -4,15 +4,16 @@ import { Component, Input } from "@angular/core";
   selector: 'app-card',
   standalone: true,
   template: `
-  <div class="card">
-    <div class="card-body d-flex flex-column align-items-center gap-3 bg-light">
-        <div style="height: 26px">
-        <h2 class="card-title fs-4 text-center">{{ title }}</h2>
+  <div class="card p-0">
+    <div class="card-body d-flex flex-column justify-content-between align-items-center gap-3 p-0">
+
+        <div style="height: 70px" class="d-flex align-items-center justify-content-center w-100 border border-0 border-bottom bg-body-tertiary rounded">
+        <h2 class="card-title fs-4 m-0 fw-medium text-center">{{ title }}</h2>
         </div>
 
-        <div style="height: 300px; background: lightblue;">
+        <div style="height: 250px;" class="w-75 d-flex justify-content-center bg-white">
         <img
-        [src]="imageUrl"class="h-75 img-fluid object-fit-cover"alt="article image">
+        [src]="imageUrl"class="h-100 img-fluid object-fit-cover"alt="article image">
         </div>
 
         <p class="card-text">{{ content }}</p>
