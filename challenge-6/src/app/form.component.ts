ab8cf0740115c2034c082cbff3896eb783b5e91c
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-11-14 12:54:46 +0100

    update

--- challenge-6/src/app/form.component.ts
+++ challenge-6/src/app/form.component.ts
@@ -9,15 +9,17 @@ import { Component } from "@angular/core";
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <small id="emailHelp" class="form-text">We'll never share your email with anyone else.</small>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
     <input type="password" class="form-control" id="exampleInputPassword1">
+    <small id="emailHelp" class="form-text">We'll never share your email with anyone else.</small>
   </div>
   <div class="mb-3">
   <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
   <textarea class="form-control" id="exampleFormControlTextarea1" rows="12"></textarea>
+  <small id="emailHelp" class="form-text">We'll never share your email with anyone else.</small>
 </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 
